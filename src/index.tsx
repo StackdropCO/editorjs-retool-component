43f7b1c3cb2630ab6e9ef714a845fdf9207b26ea
minijohn <johnminiadis@hollandandbarrett.com>
2025-03-05 10:39:18 +0200

    added more blocks

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import React, { useEffect, useRef } from 'react'
-import EditorJS from '@editorjs/editorjs'
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header'
+import List from '@editorjs/list'
+import Quote from '@editorjs/quote'
+import CodeTool from '@editorjs/code'
+import Marker from '@editorjs/marker'
+import Checklist from '@editorjs/checklist'
+import Delimiter from '@editorjs/delimiter'
+import InlineCode from '@editorjs/inline-code'
 import type { FC } from 'react'
 
 import { Retool } from '@tryretool/custom-component-support'
@@ -14,17 +22,39 @@ export const EditorComponent: FC = () => {
 
   useEffect(() => {
     if (editorRef.current && !editorInstance.current) {
-      // Initialize Editor.js with desired configuration.
       editorInstance.current = new EditorJS({
         holder: editorRef.current,
         placeholder: 'Start typing here...',
-        // Example tool configuration – you can add more tools as needed.
-        // tools: {
-        //   header: {
-        //     class: ".ads", // (await import('@editorjs/header')).default,
-        //     inlineToolbar: true
-        //   }
-        // }
+
+        tools: {
+          header: {
+            class: Header,
+            inlineToolbar: ['marker', 'link']
+          },
+          list: {
+            class: List,
+            inlineToolbar: true
+          },
+          quote: {
+            class: Quote,
+            inlineToolbar: true,
+            config: {
+              quotePlaceholder: 'Enter a quote',
+              captionPlaceholder: "Quote's author"
+            }
+          },
+          code: CodeTool,
+          marker: {
+            class: Marker,
+            shortcut: 'CMD+SHIFT+M'
+          },
+          checklist: {
+            class: Checklist,
+            inlineToolbar: true
+          },
+          delimiter: Delimiter,
+          inlineCode: InlineCode
+        }
       })
     }
 
